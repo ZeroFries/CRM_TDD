69f21e2cbaaed6c473620097df6fd681b0ef83d9
Dillon Raphael <dillonraphael@gmail.com>
2013-06-11 12:52:03 -0400

    Added ID param

--- tests/Test_contacts.rb
+++ tests/Test_contacts.rb
@@ -2,13 +2,17 @@ require_relative "Test_helper.rb"
 
 class TestContacts < Test::Unit::TestCase
 	def setup
-		@contact = Contacts.new(:first_name, "Stu", :last_name, "G", :email, "sg@sg.com")
+		@contact = Contacts.new(:last_name => "G", :first_name => "Stu", :email => "sg@sg.com")
 	end
 
 	def test_contact_has_parameters
 		assert_equal @contact.first_name, "Stu"
 		assert_equal @contact.last_name, "G"
 		assert_equal @contact.email, "sg@sg.com"
-		assert_equal @contact.notes, ""
+		assert_equal @contact.notes, " "
+	end
+
+	def test_contact_has_id
+		assert_equal @contact.id, 1
 	end
 end
